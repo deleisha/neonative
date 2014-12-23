6b69efb21318947ccd9ea45725be75f677cda9e1
deleisha <dlmeetei@gmail.com>
2014-12-24 02:58:06 +0530

    Improved and simplified TCP

--- main.cc
+++ main.cc
@@ -57,80 +57,59 @@ void Loop::stop()
 }
 
 
-namespace base {
-    class Handle {
-    public:
-        Handle() {}
-        virtual ~Handle() {}
-
-        bool is_active(void) const;
-        bool is_closing() const;
-        void close();
-        void close_handle();
-        bool has_ref();
-    private:
-    //uv_any_handle hdl;
-  //  typedef uv_handle_type HandleType;
-   // HandleType handle_type;
-
-    };
-
-    class StreamHandle : public Handle {
-    };
-
-}
-
-
-class  TCPHandle :public StreamHandle{
+class  TCP {
         public:
-        TCPHandle();
-        ~TCPHandle();
-        
+        TCP(const Loop &loop):tcp_(new uv_tcp_t)
+        {
+            uv_tcp_init(loop.get(), tcp_);
+        }
+        ~TCP()
+        {
+            if(tcp_) {
+                delete tcp_;
+                tcp_ = 0;
+            }
+        }
+
+        int listen(const sockaddr* addr, unsigned int flags) {
+            neo_bind(addr, flags);
+            return _listen(128, connection_cb);
+        }
+        protected:
+        int neo_bind(const sockaddr* addr, unsigned int flags) {
+            int r = uv_tcp_bind(tcp_, addr,flags);
+            if(r) {
+                uv_strerror(r);
+            }
+        }
+        int _listen(int backlog, uv_connection_cb cb) {
+            return uv_listen(reinterpret_cast<uv_stream_t*>(tcp_), backlog, cb);
+        }
+        static void connection_cb(uv_stream_t* stream, int status) {
+
+            std::cout << "Yahoo:" << std::endl;
+        }
+
         private:
+        uv_tcp_t* tcp_;
 };
-class Checkhandle :public Handle {
-        public:
-        Checkhandle(const Loop &loop);
-        ~Checkhandle();
-
-        int start();
-        int stop();
-        private:
-        uv_check_t *hdl_;
-    };
-
-    Checkhandle::Checkhandle(const Loop &loop) {
-        int r = uv_check_init(loop.get(), hdl_);
-    }
-
-    Checkhandle::~Checkhandle() {
-    }
-
-    int Checkhandle::start() {
-    }
-
-    int Checkhandle::stop() {
-    }
 
 
 NEO_NAMESPACE_END
 
 
-void connection_cb(uv_stream_t* srvr, int status) {
-    std::cout << "Yahoo:" << std::endl;
-}
-
 using namespace neonative;
 int main()
 {
     Loop loop;
-    uv_tcp_t server;
-    uv_tcp_init(loop.get(), &server);
+    TCP server(loop);
     struct sockaddr_in bind_addr;
-    int r = uv_ip4_addr("0.0.0.0", 7000, &bind_addr);
+    int r = uv_ip4_addr("0.0.0.0", 6000, &bind_addr);
     assert(!r);
-    uv_tcp_bind(&server, (struct sockaddr*)&bind_addr, 0);
-    r = uv_listen((uv_stream_t*) &server, 128 /*backlog*/, connection_cb);
+    r = server.listen((struct sockaddr*)&bind_addr, 0);
+    if (r) {
+        std::cout << "Error on listening" << uv_strerror(r) << std::endl;
+    }
     uv_run(loop.get(), UV_RUN_DEFAULT);
     return 0;
 }
